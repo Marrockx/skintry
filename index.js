6f56738fef9d8489478f953326cc93282ede116c
Marrockx <mariam.adekola10@gmail.com>
2022-05-16 17:05:50 +0100

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+import './semantic/dist/semantic.js';
+import './semantic/dist/semantic.min.css';
+import 'https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js';
+import '/semantic/dist/components/dropdown.js';
